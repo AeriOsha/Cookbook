e4c9ce2e36c98505eb9465d124c102de91df50a6
Aeri Osha <aeriosha@MacBook-Air.local>
2022-07-04 12:36:50 -0500

    added authentication and authorication

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
+const Review = require('./review')
 const Schema = mongoose.Schema;
 
-const CampgroundSchema = new Schema ({
-    title:String,
-    image:String,
-    price:Number,
-    description:String,
-    location:String,
+const CampgroundSchema = new Schema({
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String,
+    author:{
+        type: Schema.Types.ObjectId,
+        ref:'User'
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
@@ -15,5 +20,14 @@ const CampgroundSchema = new Schema ({
     ]
 });
 
-module.exports = mongoose.model('Campground', CampgroundSchema)
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
+module.exports = mongoose.model('Campground', CampgroundSchema);
--- schemas.js
+++ schemas.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required().alphanum().min(3),
+        title: Joi.string().required().min(3),
         price: Joi.number().required().min(0),
         image: Joi.string().required(),
         location: Joi.string().required().min(3),
